c481429a69a347f975fdb1287083e58d30add2a2
omar39516 <57820092+omar39516@users.noreply.github.com>
2023-05-24 15:58:37 -0400

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.call("pip install bottle")
+subprocess.call("pip install beaker")
+subprocess.call("pip install d2lvalence")
+subprocess.call("pip install d2lvalence_util")
